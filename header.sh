b5be15b6206a3c685076e651a6968c363646bbd4
marctormo <40198266+marctormo@users.noreply.github.com>
2018-11-14 16:22:44 +0100

    Update header.sh

--- header.sh
+++ header.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-
-#SBATCH --job-name=gatk
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=12G
+#SBATCH --job-name=fastqc
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=4G
 #SBATCH -o slurm.%j.out
 #SBATCH -e slurm.%j.err
- 
-# mail alert at start, end and abortion of execution
 #SBATCH --mail-type=END
-#SBATCH --mail-user=your.user@upf.edu
+#SBATCH --mail-user=youruser@email.com
 
-#running your commands
+#SBATCH --reservation=training
+
+module load FastQC/0.11.7-Java-1.8.0_74
 date
-sleep 60
+fastqc -o fastqc test_R1.fastq.gz test_R2.fastq.gz -t 2
 date
+
 
